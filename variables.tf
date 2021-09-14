282543d37d8f76aed35329ad09b1c82ecbb3db24
mathewseby <mathewseby.seby@gmail.com>
2021-09-15 00:38:13 +0530

    role changes

--- variables.tf
+++ variables.tf
@@ -2,4 +2,6 @@ variable "instance_key_name" {}
 variable "ssh-user" {}
 variable "private_key_path" {}
 variable "instance-type" {}
-variable "instance-ami" {}
+variable "instance-ami" {
+  default = "ami-06cf02a98a61f9f5e"
+}
