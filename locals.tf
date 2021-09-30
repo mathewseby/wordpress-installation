3c5e3d3a6e69928ca4a8af884dcda42668f44731
mathewseby <mathew.sebastian@scalar-labs.com>
2021-09-30 12:32:51 +0530

    correct format

--- locals.tf
+++ locals.tf
@@ -1,5 +1,5 @@
 locals {
-  ssh_user         = var.ssh-user
-  key_name         = var.instance_key_name
+  ssh_user = var.ssh-user
+  key_name = var.instance_key_name
   #private_key_path = var.private_key_path
 }
