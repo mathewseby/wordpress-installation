66f54fe52d8cff19968461b18564f0bfa1ab9b16
mathewseby <mathew.sebastian@scalar-labs.com>
2021-10-26 23:46:20 +0530

    test all options (rds-5.7)

--- rds.tf
+++ rds.tf
@@ -3,7 +3,7 @@ resource "aws_db_instance" "wp-rds" {
   identifier           = "wpdb"
   allocated_storage    = 10
   engine               = "mysql"
-  engine_version       = "8.0"
+  engine_version       = "5.7"
   instance_class       = "db.t3.micro"
   name                 = "wpdatabase"
   username             = "root"
