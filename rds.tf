7c64fdb5dbfcaffdc4e21dac91a6065bd324800f
mathewseby <mathew.sebastian@scalar-labs.com>
2021-10-30 21:17:01 +0530

    test all options (rds-8.0)

--- rds.tf
+++ rds.tf
@@ -3,12 +3,12 @@ resource "aws_db_instance" "wp-rds" {
   identifier           = "wpdb"
   allocated_storage    = 10
   engine               = "mysql"
-  engine_version       = "5.7"
+  engine_version       = "8.0"
   instance_class       = "db.t3.micro"
   name                 = "wpdatabase"
   username             = "root"
   password             = "N$=]:%9;F7z%DX!X"
-  parameter_group_name = "default.mysql5.7"
+  parameter_group_name = "default.mysql8.0"
   skip_final_snapshot  = true
   vpc_security_group_ids = [
     "${one(aws_security_group.db_sg[*].id)}"
