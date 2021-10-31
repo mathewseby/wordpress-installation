7696d1da5e00a39dcc935e8d48f8887c53badbc0
mathewseby <mathew.sebastian@scalar-labs.com>
2021-10-31 13:10:08 +0530

    install type sed

--- ec2.tf
+++ ec2.tf
@@ -28,13 +28,13 @@ resource "aws_instance" "wp-instance" {
 resource "null_resource" "rds-server-provision" {
   count = var.install_type == "server_with_rds" || var.install_type == "with_docker_rds" ? 1 : 0
   provisioner "local-exec" {
-    command = "sed -i 's/^install_type:/install_type: ${var.install_type}/g' playbooks/install-wordpress.yml ; sed -i 's/rdshost: localhost/rdshost: ${one(aws_db_instance.wp-rds[*].endpoint)}/g' playbooks/roles/wordpress/defaults/main.yml ; sed -i 's/:3306//g' playbooks/roles/wordpress/defaults/main.yml ; ansible-playbook -i ${aws_instance.wp-instance.public_ip}, -u ${var.ssh-user} playbooks/install-wordpress.yml ; sleep 30s ; curl -I ${aws_instance.wp-instance.public_ip}"
+    command = "sed -i 's/.*install_type: .*/   install_type: ${var.install_type}/g' playbooks/install-wordpress.yml ; sed -i 's/.*rdshost: .*/rdshost: ${one(aws_db_instance.wp-rds[*].endpoint)}/g' playbooks/roles/wordpress/defaults/main.yml ; sed -i 's/:3306//g' playbooks/roles/wordpress/defaults/main.yml ; ansible-playbook -i ${aws_instance.wp-instance.public_ip}, -u ${var.ssh-user} playbooks/install-wordpress.yml ; sleep 30s ; curl -I ${aws_instance.wp-instance.public_ip}"
   }
 }
 resource "null_resource" "rds-nill-provision" {
   count = var.install_type == "server" || var.install_type == "docker" ? 1 : 0
   provisioner "local-exec" {
-    command = "sed -i 's/^install_type:/install_type: ${var.install_type}/g' playbooks/install-wordpress.yml ; ansible-playbook -i ${aws_instance.wp-instance.public_ip}, -u ${var.ssh-user} playbooks/install-wordpress.yml ; sleep 30s ; curl -I ${aws_instance.wp-instance.public_ip}"
+    command = "sed -i 's/.*install_type: .*/   install_type: ${var.install_type}/g' playbooks/install-wordpress.yml ; ansible-playbook -i ${aws_instance.wp-instance.public_ip}, -u ${var.ssh-user} playbooks/install-wordpress.yml ; sleep 30s ; curl -I ${aws_instance.wp-instance.public_ip}"
   }
 }
 
