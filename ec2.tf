2ba4ac8a2ba15dc1c3fdf20714273012d535e7b6
mathewseby <mathew.sebastian@scalar-labs.com>
2021-10-31 17:43:59 +0530

    add private ip output

--- ec2.tf
+++ ec2.tf
@@ -28,7 +28,7 @@ resource "aws_instance" "wp-instance" {
 resource "null_resource" "rds-server-provision" {
   count = var.install_type == "server_with_rds" || var.install_type == "with_docker_rds" ? 1 : 0
   provisioner "local-exec" {
-    command = "sed -i 's/.*install_type: .*/   install_type: ${var.install_type}/g' playbooks/install-wordpress.yml ; sed -i 's/.*dbuserhost: .*/dbuserhost: ${"var.dbuserhost"}/g' playbooks/roles/wordpress/defaults/main.yml ; sed -i 's/.*mysql_root_password: .*/mysql_root_password: ${var.mysql_root_password}/g' playbooks/roles/wordpress/defaults/main.yml ; sed -i 's/.*rdshost: .*/rdshost: ${one(aws_db_instance.wp-rds[*].endpoint)}/g' playbooks/roles/wordpress/defaults/main.yml ; sed -i 's/:3306//g' playbooks/roles/wordpress/defaults/main.yml ; ansible-playbook -i ${aws_instance.wp-instance.public_ip}, -u ${var.ssh-user} playbooks/install-wordpress.yml ; sleep 30s ; curl -I ${aws_instance.wp-instance.public_ip}"
+    command = "sed -i 's/.*install_type: .*/   install_type: ${var.install_type}/g' playbooks/install-wordpress.yml ; sed -i 's/.*dbuserhost: .*/dbuserhost: '${var.dbuserhost}'/g' playbooks/roles/wordpress/defaults/main.yml ; sed -i 's/.*mysql_root_password: .*/mysql_root_password: ${var.mysql_root_password}/g' playbooks/roles/wordpress/defaults/main.yml ; sed -i 's/.*rdshost: .*/rdshost: ${one(aws_db_instance.wp-rds[*].endpoint)}/g' playbooks/roles/wordpress/defaults/main.yml ; sed -i 's/:3306//g' playbooks/roles/wordpress/defaults/main.yml ; ansible-playbook -i ${aws_instance.wp-instance.public_ip}, -u ${var.ssh-user} playbooks/install-wordpress.yml ; sleep 30s ; curl -I ${aws_instance.wp-instance.public_ip}"
   }
 }
 
@@ -41,4 +41,8 @@ resource "null_resource" "rds-nill-provision" {
 
 output "ec2_public-ip" {
   value = aws_instance.wp-instance.public_ip
+}
+
+output "ec2-private-ip" {
+  value = aws_instance.wp-instance.private_ip
 }
