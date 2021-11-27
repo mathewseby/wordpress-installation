408a536b0aecb4071183ad99226df8708172741c
mathewseby <mathew.sebastian@engati.com>
2021-11-27 17:53:03 +0530

    ecs sg and vpc sections

--- security-group.tf
+++ security-group.tf
@@ -68,3 +68,64 @@ resource "aws_security_group_rule" "db-inbound" {
   security_group_id        = one(aws_security_group.db_sg[*].id)
   source_security_group_id = aws_security_group.ec2_sg.id
 }
+
+resource "aws_security_group" "lb-sg" {
+  count       = var.install_type == "ecs" ? 1 : 0
+  name        = "ls_sg"
+  description = "for alb"
+  vpc_id      = aws_vpc.vpc.id
+}
+
+resource "aws_security_group_rule" "lb-inbound" {
+  count             = var.install_type == "ecs" ? 1 : 0
+  type              = "ingress"
+  from_port         = 80
+  to_port           = 80
+  protocol          = "tcp"
+  security_group_id = one(aws_security_group.ecs-sg[*].id)
+  cidr_blocks       = ["0.0.0.0/0"]
+}
+
+resource "aws_security_group_rule" "lb-outbound" {
+  count                    = var.install_type == "ecs" ? 1 : 0
+  type                     = "ingress"
+  from_port                = 80
+  to_port                  = 80
+  protocol                 = "tcp"
+  security_group_id        = one(aws_security_group.lb-sg[*].id)
+  source_security_group_id = aws_security_group.ecs-sg.id
+}
+
+resource "aws_security_group" "ecs-sg" {
+  count       = var.install_type == "ecs" ? 1 : 0
+  name        = "db_sg"
+  description = "for ecs"
+  vpc_id      = aws_vpc.vpc.id
+}
+
+resource "aws_security_group_rule" "ecs-inbound" {
+  count                    = var.install_type == "ecs" ? 1 : 0
+  type                     = "ingress"
+  from_port                = 80
+  to_port                  = 80
+  protocol                 = "tcp"
+  security_group_id        = one(aws_security_group.ecs-sg[*].id)
+  source_security_group_id = aws_security_group.lb-sg.id
+}
+
+resource "aws_security_group" "efs-sg" {
+  count       = var.install_type == "ecs" ? 1 : 0
+  name        = "efs_sg"
+  description = "for efs"
+  vpc_id      = aws_vpc.vpc.id
+}
+
+resource "aws_security_group_rule" "efs-inbound" {
+  count                    = var.install_type == "ecs" ? 1 : 0
+  type                     = "ingress"
+  from_port                = 2049
+  to_port                  = 2049
+  protocol                 = "tcp"
+  security_group_id        = one(aws_security_group.efs-sg[*].id)
+  source_security_group_id = aws_security_group.ecs-sg.id
+}
