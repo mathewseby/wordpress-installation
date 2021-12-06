5a4b219168f82e19f89aa51c9685815e7bda296c
mathewseby <mathew.sebastian@scalar-labs.com>
2021-12-06 20:52:40 +0530

    enable efs and ecs subnets

--- vpc.tf
+++ vpc.tf
@@ -15,11 +15,11 @@ resource "aws_subnet" "ecs-01" {
   vpc_id     = aws_vpc.vpc.id
 }
 
-#resource "aws_subnet" "ecs-02" {
-#  count      = var.install_type == "ecs" ? 1 : 0
-#  cidr_block = "172.20.3.0/24"
-#  vpc_id     = aws_vpc.vpc.id
-#}
+resource "aws_subnet" "ecs-02" {
+  count      = var.install_type == "ecs" ? 1 : 0
+  cidr_block = "172.20.3.0/24"
+  vpc_id     = aws_vpc.vpc.id
+}
 
 resource "aws_subnet" "efs-01" {
   count      = var.install_type == "ecs" ? 1 : 0
@@ -123,18 +123,18 @@ resource "aws_route_table_association" "private-05" {
   subnet_id      = one(aws_subnet.efs-02[*].id)
 }
 
-#resource "aws_route_table_association" "private-06" {
-#  count          = var.install_type == "ecs" ? 1 : 0
-#  route_table_id = one(aws_route_table.private[*].id)
-#  subnet_id      = one(aws_subnet.ecs-01[*].id)
-#}
-#
-#resource "aws_route_table_association" "private-07" {
-#  count          = var.install_type == "ecs" ? 1 : 0
-#  route_table_id = one(aws_route_table.private[*].id)
-#  subnet_id      = one(aws_subnet.ecs-02[*].id)
-#}
-#
+resource "aws_route_table_association" "private-06" {
+  count          = var.install_type == "ecs" ? 1 : 0
+  route_table_id = one(aws_route_table.private[*].id)
+  subnet_id      = one(aws_subnet.ecs-01[*].id)
+}
+
+resource "aws_route_table_association" "private-07" {
+  count          = var.install_type == "ecs" ? 1 : 0
+  route_table_id = one(aws_route_table.private[*].id)
+  subnet_id      = one(aws_subnet.ecs-02[*].id)
+}
+
 #resource "aws_db_subnet_group" "db" {
 #  count      = var.install_type == "server_with_rds" || var.install_type == "with_docker_rds" || var.install_type == "ecs" ? 1 : 0
 #  subnet_ids = [one(aws_subnet.db-02[*].id), one(aws_subnet.db-01[*].id)]
