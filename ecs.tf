d9b3b3b35027a974c24652fb4919a61cd71233d4
mathewseby <mathew.sebastian@scalar-labs.com>
2021-12-12 19:44:20 +0530

    ecs fix

--- ecs.tf
+++ ecs.tf
@@ -6,12 +6,13 @@ resource "aws_ecs_task_definition" "service" {
   family = "service"
   container_definitions = jsonencode([
     {
-      name         = "wp"
-      image        = "registry.hub.docker.com/library/wordpress:latest"
-      cpu          = 10
-      memory       = 512
-      essential    = true
-      network_mode = "awsvpc"
+      name                     = "wp"
+      image                    = "registry.hub.docker.com/library/wordpress:latest"
+      cpu                      = 10
+      memory                   = 512
+      essential                = true
+      network_mode             = "awsvpc"
+      requires_compatibilities = "FARGATE"
       portMappings = [
         {
           containerPort = 80
