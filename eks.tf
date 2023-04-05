5bae830a3bcce213479feb2e0dd904622997b4a7
mathewseby <mathew.sebastian@scalar-labs.com>
2023-04-06 00:51:26 +0530

    add nodegroups

--- eks.tf
+++ eks.tf
@@ -1,9 +1,21 @@
 resource "aws_eks_cluster" "wp_eks" {
-  count      = var.install_type == "eks" ? 1 : 0
+  #count      = var.install_type == "eks" ? 1 : 0
   name       = "wp-eks"
   role_arn   = aws_iam_role.eksclusterrole.arn
-  depends_on = [aws_iam_role.eksclusterrole]
+  depends_on = [aws_iam_role.eksclusterrole, aws_iam_role.eksnoderole]
   vpc_config {
     subnet_ids = ["${one(aws_subnet.eks-01[*].id)}", "${one(aws_subnet.eks-02[*].id)}"]
+  }
+}
+
+resource "aws_eks_node_group" "wp-eks-nodes" {
+  cluster_name    = aws_eks_cluster.wp_eks.name
+  node_group_name = "wp-nodegroup"
+  node_role_arn   = aws_iam_role.eksnoderole.arn
+  subnet_ids      = ["${one(aws_subnet.eks-01[*].id)}", "${one(aws_subnet.eks-02[*].id)}"]
+  scaling_config {
+    desired_size = 1
+    max_size     = 1
+    min_size     = 1
   }
 }
--- iam.tf
+++ iam.tf
@@ -17,6 +17,21 @@ resource "aws_iam_role" "eksclusterrole" {
   managed_policy_arns = [data.aws_iam_policy.ekspolicy.arn]
 }
 
+resource "aws_iam_role" "eksnoderole" {
+  name                = "eksnoderole"
+  managed_policy_arns = [data.aws_iam_policy.eksworker.arn, data.aws_iam_policy.containerregistry.arn, data.aws_iam_policy.ekscni.arn]
+}
+
 data "aws_iam_policy" "ekspolicy" {
   arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+}
+
+data "aws_iam_policy" "eksworker" {
+  arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+}
+data "aws_iam_policy" "containerregistry" {
+  arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+}
+data "aws_iam_policy" "ekscni" {
+  arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
 }
