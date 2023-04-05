cfbaae4705436db7f9180f8a5be1195601fbb24d
mathewseby <mathew.sebastian@scalar-labs.com>
2023-04-06 00:09:25 +0530

    iam fix

--- eks.tf
+++ eks.tf
@@ -1,9 +1,9 @@
-//resource "aws_eks_cluster" "wp_eks" {
-//  count      = var.install_type == "eks" ? 1 : 0
-//  name       = "wp-eks"
-//  role_arn   = "aws_iam_role.eksclusterrole.arn"
-//  depends_on = [aws_iam_role.eksclusterrole]
-//  vpc_config {
-//    subnet_ids = ["${one(aws_subnet.eks-01[*].id)}", "${one(aws_subnet.eks-02[*].id)}"]
-//  }
-//}
+resource "aws_eks_cluster" "wp_eks" {
+  count      = var.install_type == "eks" ? 1 : 0
+  name       = "wp-eks"
+  role_arn   = aws_iam_role.eksclusterrole.arn
+  depends_on = [aws_iam_role.eksclusterrole]
+  vpc_config {
+    subnet_ids = ["${one(aws_subnet.eks-01[*].id)}", "${one(aws_subnet.eks-02[*].id)}"]
+  }
+}
