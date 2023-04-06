f7bfc3e688556a11a04ac768c067cfa65d49517a
mathewseby <mathew.sebastian@engati.com>
2023-04-06 16:46:18 +0530

    eks sg changes

--- security-group.tf
+++ security-group.tf
@@ -43,6 +43,7 @@ resource "aws_security_group_rule" "ec2-out-https" {
 }
 
 resource "aws_security_group" "db-sg" {
+  count       = var.install_type == "server_with_rds" || var.install_type == "with_docker_rds" || var.install_type == "eks" ? 1 : 0
   name        = "db_sg"
   description = "for database instance"
   vpc_id      = aws_vpc.vpc.id
@@ -75,89 +76,55 @@ resource "aws_security_group_rule" "db-inbound-eks" {
   to_port                  = 3306
   protocol                 = "tcp"
   security_group_id        = one(aws_security_group.db-sg[*].id)
-  source_security_group_id = one(aws_security_group.ecs-sg[*].id)
+  source_security_group_id = one(aws_security_group.eks-sg[*].id)
 
 }
 
-resource "aws_security_group" "lb-sg" {
-  count       = var.install_type == "ecs" ? 1 : 0
-  name        = "lb_sg"
-  description = "for alb"
-  vpc_id      = aws_vpc.vpc.id
-}
-
-resource "aws_security_group_rule" "lb-inbound" {
-  count             = var.install_type == "ecs" ? 1 : 0
-  type              = "ingress"
-  from_port         = 80
-  to_port           = 80
-  protocol          = "tcp"
-  security_group_id = one(aws_security_group.ecs-sg[*].id)
-  cidr_blocks       = ["0.0.0.0/0"]
-}
-
-resource "aws_security_group_rule" "lb-outbound" {
-  count                    = var.install_type == "ecs" ? 1 : 0
-  type                     = "ingress"
-  from_port                = 80
-  to_port                  = 80
-  protocol                 = "tcp"
-  security_group_id        = one(aws_security_group.lb-sg[*].id)
-  source_security_group_id = one(aws_security_group.ecs-sg[*].id)
-}
-
-resource "aws_security_group" "ecs-sg" {
-  count       = var.install_type == "ecs" ? 1 : 0
+resource "aws_security_group" "eks-sg" {
+  count       = var.install_type == "eks" ? 1 : 0
   name        = "ecs-sg"
   description = "for ecs"
   vpc_id      = aws_vpc.vpc.id
 }
 
-resource "aws_security_group_rule" "ecs-inbound" {
-  count                    = var.install_type == "ecs" ? 1 : 0
-  type                     = "ingress"
-  from_port                = 80
-  to_port                  = 80
-  protocol                 = "tcp"
-  security_group_id        = one(aws_security_group.ecs-sg[*].id)
-  source_security_group_id = one(aws_security_group.lb-sg[*].id)
+resource "aws_security_group_rule" "eks-inbound" {
+  count             = var.install_type == "eks" ? 1 : 0
+  type              = "ingress"
+  from_port         = 80
+  to_port           = 80
+  protocol          = "tcp"
+  security_group_id = one(aws_security_group.eks-sg[*].id)
+  cidr_blocks       = ["0.0.0.0/0"]
 }
 
-resource "aws_security_group" "efs-sg" {
-  count       = var.install_type == "ecs" ? 1 : 0
-  name        = "efs_sg"
-  description = "for efs"
-  vpc_id      = aws_vpc.vpc.id
+resource "aws_security_group_rule" "eks-inbound" {
+  count             = var.install_type == "eks" ? 1 : 0
+  type              = "ingress"
+  from_port         = 443
+  to_port           = 443
+  protocol          = "tcp"
+  security_group_id = one(aws_security_group.eks-sg[*].id)
+  cidr_blocks       = ["0.0.0.0/0"]
 }
 
-resource "aws_security_group_rule" "efs-inbound" {
-  count                    = var.install_type == "ecs" ? 1 : 0
-  type                     = "ingress"
-  from_port                = 2049
-  to_port                  = 2049
-  protocol                 = "tcp"
-  security_group_id        = one(aws_security_group.efs-sg[*].id)
-  source_security_group_id = one(aws_security_group.ecs-sg[*].id)
-}
-
-resource "aws_security_group_rule" "ecs-out" {
-  count             = var.install_type == "ecs" ? 1 : 0
+resource "aws_security_group_rule" "eks-out" {
+  count             = var.install_type == "eks" ? 1 : 0
   type              = "egress"
   from_port         = 80
   to_port           = 80
   protocol          = "tcp"
-  security_group_id = one(aws_security_group.ecs-sg[*].id)
+  security_group_id = one(aws_security_group.eks-sg[*].id)
   cidr_blocks       = ["0.0.0.0/0"]
 
 }
 
-resource "aws_security_group_rule" "ecs-out-https" {
-  count             = var.install_type == "ecs" ? 1 : 0
+resource "aws_security_group_rule" "eks-out-https" {
+  count             = var.install_type == "eks" ? 1 : 0
   type              = "egress"
   from_port         = 443
   to_port           = 443
   protocol          = "tcp"
-  security_group_id = one(aws_security_group.ecs-sg[*].id)
+  security_group_id = one(aws_security_group.eks-sg[*].id)
   cidr_blocks       = ["0.0.0.0/0"]
 
 }
@@ -168,6 +135,6 @@ resource "aws_security_group_rule" "eks-db-outbound" {
   from_port                = 3306
   to_port                  = 3306
   protocol                 = "tcp"
-  security_group_id        = one(aws_security_group.ecs-sg[*].id)
+  security_group_id        = one(aws_security_group.eks-sg[*].id)
   source_security_group_id = one(aws_security_group.db-sg[*].id)
 }
