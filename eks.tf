f2b1a0c0c80df9773edef67bb7260ec16c391851
mathewseby <mathew.sebastian@scalar-labs.com>
2023-04-13 02:27:21 +0530

    helm fix

--- eks.tf
+++ eks.tf
@@ -2,7 +2,7 @@ resource "aws_eks_cluster" "wp_eks" {
   count      = var.install_type == "eks" ? 1 : 0
   name       = "wp-eks"
   role_arn   = one(aws_iam_role.eksclusterrole[*].arn)
-  depends_on = [aws_iam_role.eksclusterrole, aws_iam_role.eksnoderole]
+  depends_on = [aws_iam_role.eksclusterrole, aws_iam_role.eksnoderole, aws_db_instance.wp-rds]
   vpc_config {
     subnet_ids         = ["${one(aws_subnet.eks-01[*].id)}", "${one(aws_subnet.eks-02[*].id)}"]
     security_group_ids = ["${one(aws_security_group.eks-sg[*].id)}"]
