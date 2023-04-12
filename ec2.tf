871a0c9e259061fb220a773371fa2e0d00a107cd
mathewseby <mathew.sebastian@scalar-labs.com>
2023-04-13 00:56:47 +0530

    add helm install steps

--- ec2.tf
+++ ec2.tf
@@ -28,7 +28,8 @@ resource "aws_instance" "wp-instance" {
 }
 
 resource "null_resource" "eks-rds-provision" {
-  count = var.install_type == "eks" ? 1 : 0
+  count      = var.install_type == "eks" ? 1 : 0
+  depends_on = [aws_eks_cluster.wp_eks]
   provisioner "local-exec" {
     command = "export ANSIBLE_HOST_KEY_CHECKING=False ; sed -i 's/.*install_type: .*/   install_type: ${var.install_type}/g' playbooks/install-wordpress.yml ; sed -i 's/.*mysql_root_password: .*/mysql_root_password: ${var.mysql_root_password}/g' playbooks/roles/wordpress/defaults/main.yml ; sed -i 's/.*rdshost: .*/rdshost: ${one(aws_db_instance.wp-rds[*].endpoint)}/g' playbooks/roles/wordpress/defaults/main.yml ; sed -i 's/:3306//g' playbooks/roles/wordpress/defaults/main.yml ; ansible-playbook -i ${one(aws_instance.wp-instance[*].public_ip)}, -u ${var.ssh-user} playbooks/install-wordpress.yml"
   }
