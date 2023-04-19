affe3b85a26233952ee6a467450f2462baf59e2a
mathewseby <mathew.sebastian@engati.com>
2023-04-19 16:54:01 +0530

    add eks

--- eks.tf
+++ eks.tf
@@ -1,10 +1,10 @@
 module "eks" {
   source       = "./modules/eks"
-  kube_subnets = [aws_subnet.eks-01, aws_subnet.eks-02]
+  kube_subnets = [aws_subnet.eks-01.id, aws_subnet.eks-02.id]
   kube_sgs     = [aws_security_group.ec2_sg.id]
 }
 
 module "wp-eks-nodes" {
   source       = "./modules/eks"
-  kube_subnets = [aws_subnet.eks-01, aws_subnet.eks-02]
+  kube_subnets = [aws_subnet.eks-01.id, aws_subnet.eks-02.id]
 }
