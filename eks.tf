c69e065ff7cf76b0e18017dfc711f3a9387ff735
mathewseby <mathew.sebastian@scalar-labs.com>
2023-05-28 01:06:15 +0530

    add kubeconfig

--- eks.tf
+++ eks.tf
@@ -53,7 +53,8 @@ module "eks" {
 }
 
 data "aws_eks_cluster" "default" {
-  name = module.eks.cluster_name
+  depends_on = [module.eks]
+  name       = module.eks.cluster_name
 }
 
 data "aws_eks_cluster_auth" "default" {
